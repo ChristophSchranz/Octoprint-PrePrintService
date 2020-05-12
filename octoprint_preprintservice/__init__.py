a262e592eed31b515368bb9b380f111c57c456d9
christoph.schranz@salzburgresearch.at <christoph.schranz@salzburgresearch.at>
2020-05-12 15:47:13 +0200

    removing "Using PrePrintserver" in navbar

--- octoprint_preprintservice/__init__.py
+++ octoprint_preprintservice/__init__.py
@@ -55,7 +55,7 @@ class PreprintservicePlugin(octoprint.plugin.SlicerPlugin,
 
 	def get_template_configs(self):
 		return [
-			dict(type="navbar", custom_bindings=False),
+			# dict(type="navbar", custom_bindings=False),
 			dict(type="settings", custom_bindings=True)
 			# dict(type="sidebar", template="dialogs/preprintservice_sidebar.jinja2", custom_bindings=True)
 		]
